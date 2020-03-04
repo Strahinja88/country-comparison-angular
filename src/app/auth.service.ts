037b65ff3c28ce90b9e7ac0f01db6b5f0a909d26
Strahinja <strahinjapantovic@ymail.com>
2020-03-04 15:06:53 +0100

    responsive

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -8,27 +8,13 @@ import { Router } from "@angular/router";
 export class AuthService {
   userData: any;
 
-  constructor(public router: Router, public users: UserService) {
-    // Setting logged in user in localstorage else null
-    // this.users.getUsers().subscribe(user => {
-    //   if (user) {
-    //     this.userData = user;
-    //     localStorage.setItem("user", JSON.stringify(this.userData));
-    //     JSON.parse(localStorage.getItem("user"));
-    //   } else {
-    //     localStorage.setItem("user", null);
-    //     JSON.parse(localStorage.getItem("user"));
-    //   }
-    // });
-  }
+  constructor(public router: Router, public users: UserService) {}
 
-  // Returns true when user is looged in and email is verified
   get isLoggedIn(): boolean {
     const user = JSON.parse(localStorage.getItem("user"));
     return user !== null ? true : false;
   }
 
-  // Sign out
   logout() {
     localStorage.removeItem("user");
     this.router.navigate(["login"]);
